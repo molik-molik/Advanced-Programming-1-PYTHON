e625fddcb190a39c0af59ceddc6e444127ab6e35
molik-molik <96942534+molik-molik@users.noreply.github.com>
2022-09-08 16:25:40 +0600

    Add files via upload

---
+++ task_1.1(Practice 2 - independent work).py
@@ -0,0 +1,21 @@
+import math
+a = int(input("Input value of a: "))
+b = int(input("Input the value of b: "))
+h = int(input("Input the h: "))
+s = ((a**2+b)*h) / (2*(a-b)+4)
+print("The answer is: ", s)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
